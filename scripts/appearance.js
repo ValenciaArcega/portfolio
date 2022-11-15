e883ec31bcc9f8ee5537ba8c00b47a81aa2d8aa2
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-14 21:56:51 -0600

    features: dark mode was completely implemented

---
+++ scripts/appearance.js
@@ -0,0 +1,21 @@
+const root = document.querySelector(':root');
+const btnDarkMode = document.querySelector('.btnDark');
+const btnLightMode = document.querySelector('.btnLight');
+btnLightMode.classList.add('hidden');
+
+const turnDark = () => {
+  root.style.setProperty('--white','#121212');
+  root.style.setProperty('--black', 'white');
+  btnDarkMode.classList.add('hidden');
+  btnLightMode.classList.remove('hidden');
+};
+
+const turnLight = () => {
+  root.style.setProperty('--white','white');
+  root.style.setProperty('--black','#121212');
+  btnLightMode.classList.add('hidden');
+  btnDarkMode.classList.remove('hidden');
+};
+
+btnDarkMode.addEventListener('click', turnDark);
+btnLightMode.addEventListener('click', turnLight);
