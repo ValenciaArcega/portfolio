d6755fdfd32ce558f70acf51e25e2cf380ed1b6e
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-16 16:45:55 -0600

    features: modals structure was completely redefined using JavaScript an CustomeElements to reuse code

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,4 +1,69 @@
 'use strict';
+
+class Modals extends HTMLElement {
+  constructor() {
+    super();
+    this.modal_number;
+    this.header_color;
+    this.modal_title;
+    this.skill_one;
+    this.skill_two;
+    this.skill_three;
+    this.skill_four;    
+  }
+
+  // to set the name of the attributes that will be in the html tag
+  static get observedAttributes() {
+    return ['number','color','title', 's1', 's2', 's3', 's4'];
+  }
+
+  // compare the html attributes wiv our local variables
+  attributeChangedCallback(Attr, oldValue, newValue) {      
+    if (Attr == "number") {
+      this.modal_number = newValue;
+    }
+    if (Attr == "color") {
+      this.header_color = newValue;
+    }
+    if (Attr == "title") {
+      this.modal_title = newValue;
+    }
+    if (Attr == "s1") {
+      this.skill_one = newValue;
+    }
+    if (Attr == "s2") {
+      this.skill_two = newValue;
+    }
+    if (Attr == "s3") {
+      this.skill_three = newValue;
+    }
+    if (Attr == "s4") {
+      this.skill_four = newValue;
+    }
+  }
+
+  // everything here will be display in the browser
+  connectedCallback() {
+    this.innerHTML =
+    `<div class="${this.modal_number} hidden">
+        <div class="container-modal-header ${this.header_color}">
+          <h1>${this.modal_title}</h1>
+          <button type="button" class="btn-closeModal">&times;</button>
+        </div>
+        <ul class="ul-modal">
+          <li>${this.skill_one}</li>
+          <li>${this.skill_two}</li>
+          <li>${this.skill_three}</li>
+          <li>${this.skill_four}</li>
+        </ul>
+    </div>      
+      `;
+  }
+}
+
+// Define el componente de la clase que ya creamos
+//                            dos palabras en el nombre
+window.customElements.define("modal-skill", Modals);
 /*
 const btnClose = document.querySelector('.btn-closeModal');
 const belowModal = document.querySelector('.below-modal');
