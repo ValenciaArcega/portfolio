2f57ad97bc0d232b8d63a4f58418eddc96ab77ec
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-16 11:54:31 -0600

    features: implementing each modal window per button with JavaScript

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,15 @@
 'use strict';
-
+/*
 const btnClose = document.querySelector('.btn-closeModal');
-const btnShow = document.querySelectorAll('.card-knowledges');
 const belowModal = document.querySelector('.below-modal');
 const modalSkills = document.querySelector('.modal-skills');
+const btnShow = document.querySelectorAll('.card-knowledges');
+const headerModal = document.querySelector('.container-modal-header');
 
 const openModal = function() {
   modalSkills.classList.remove('hidden');  
   belowModal.classList.remove('hidden'); 
+  headerModal.classList.add('msTwo');
 }
 
 const closeModal = function() {
@@ -16,14 +18,47 @@ const closeModal = function() {
 }
 
 for (let i = 0; i < btnShow.length; i++) {
-  btnShow[i].addEventListener('click',openModal);  
+  btnShow[i].addEventListener('click',openModal);
 }
 
 btnClose.addEventListener('click', closeModal);
 belowModal.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', (e/*this is the key paramether */) => {
+document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && !modalSkills.classList.contains('hidden'))  {
+      closeModal();
+  }
+}); */
+
+const btnClose = document.querySelector('.btn-closeModal');
+const belowModal = document.querySelector('.below-modal');
+const headerColor = document.querySelector('.container-modal-header');
+
+
+document.querySelector('.kn1').addEventListener('click', () => {
+  belowModal.classList.remove('hidden');
+  document.querySelector('.modal-skillOne').classList.remove('hidden');
+  headerColor.style.background = 'linear-gradient(to top left, #12b886, #38d9a9)';
+});
+
+document.querySelector('.kn2').addEventListener('click', () => {
+  belowModal.classList.remove('hidden');
+  modalSkill.classList.remove('hidden');
+  headerColor.style.background = 'linear-gradient(to top left,#fcc419, #ffd43b)';
+});
+
+
+
+// Close every modal
+const closeModal = () => {
+  belowModal.classList.add('hidden');
+  document.querySelector('.modal-skillOne').classList.add('hidden');
+}
+
+btnClose.addEventListener('click', closeModal);
+belowModal.addEventListener('click', closeModal);
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !document.querySelector('.modal-skillOne').classList.contains('hidden'))  {
       closeModal();
   }
 });
