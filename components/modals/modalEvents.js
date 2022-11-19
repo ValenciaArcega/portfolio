227fb06cad29fc8b6c39b5b805ab0a474d8e05aa
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2022-11-18 19:49:35 -0600

    fixes: the method to pop up modal windows was completely modified to get more performance and less code, using for loop

--- components/modals/modalEvents.js
+++ components/modals/modalEvents.js
@@ -1,71 +1,6 @@
 'use strict';
 
-class Modals extends HTMLElement {
-  constructor() {
-    super();
-    this.modal_number;
-    this.header_color;
-    this.modal_title;
-    this.skill_one;
-    this.skill_two;
-    this.skill_three;
-    this.skill_four;    
-  }
-
-  // to set the name of the attributes that will be in the html tag
-  static get observedAttributes() {
-    return ['number','color','title', 's1', 's2', 's3', 's4'];
-  }
-
-  // compare the html attributes wiv our local variables
-  attributeChangedCallback(Attr, oldValue, newValue) {      
-    if (Attr == "number") {
-      this.modal_number = newValue;
-    }
-    if (Attr == "color") {
-      this.header_color = newValue;
-    }
-    if (Attr == "title") {
-      this.modal_title = newValue;
-    }
-    if (Attr == "s1") {
-      this.skill_one = newValue;
-    }
-    if (Attr == "s2") {
-      this.skill_two = newValue;
-    }
-    if (Attr == "s3") {
-      this.skill_three = newValue;
-    }
-    if (Attr == "s4") {
-      this.skill_four = newValue;
-    }
-  }
-
-  // everything here will be display in the browser
-  connectedCallback() {
-    this.innerHTML =
-    `<div class="${this.modal_number} hidden">
-        <div class="container-modal-header ${this.header_color}">
-          <h1>${this.modal_title}</h1>
-          <button type="button" class="btn-closeModal">&times;</button>
-        </div>
-        <ul class="ul-modal">
-          <li>${this.skill_one}</li>
-          <li>${this.skill_two}</li>
-          <li>${this.skill_three}</li>
-          <li>${this.skill_four}</li>
-        </ul>
-    </div>      
-      `;
-  }
-}
-window.customElements.define("modal-skill", Modals);
-
-
-/*
-// Modals logic
-const belowModal = document.querySelector('.below-modal');
+/*const belowModal = document.querySelector('.below-modal');
 const btnClose = document.querySelectorAll('.btn-closeModal');
 
 const kn1 = document.querySelector('.kn1');
@@ -76,9 +11,6 @@ const kn5 = document.querySelector('.kn5');
 const kn6 = document.querySelector('.kn6');
 const kn7 = document.querySelector('.kn7');
 const kn8 = document.querySelector('.kn8');
-
-
-
 
 const msOne = document.querySelector('.modal-skill-One');
 const msTwo = document.querySelector('.modal-skill-Two');
@@ -152,7 +84,29 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && !msOne.classList.contains('hidden') || !msTwo.classList.contains('hidden') || !msThree.classList.contains('hidden') || !msFour.classList.contains('hidden') || !msFive.classList.contains('hidden') || !msSix.classList.contains('hidden') || !msSeven.classList.contains('hidden') || !msEight.classList.contains('hidden')) {
     closeModal();
   }
-});
+}); */
+
+const belowModal = document.querySelector('.below-modal');
+const btnsCloseModal = document.querySelectorAll('.btn-closeModal');
+const buttons = document.querySelectorAll('.kn1, .kn2, .kn3, .kn4, .kn5, .kn6, .kn7, .kn8');
+const modalWindow = document.querySelectorAll('.modal-skill-One, .modal-skill-Two, .modal-skill-Three, .modal-skill-Four, .modal-skill-Five, .modal-skill-Six, .modal-skill-Seven, .modal-skill-Eight');
 
 
-*/
+for (let i = 0; i < buttons.length+1; i++) {
+  const closeModal = () => {
+    belowModal.classList.add('hidden');
+    modalWindow[i].classList.add('hidden');
+  };
+  btnsCloseModal[i].addEventListener('click',closeModal);
+  belowModal.addEventListener('click', closeModal);
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modalWindow[i].classList.contains('hidden')) {
+      closeModal();
+    }
+  });  
+
+  buttons[i].addEventListener('click', function () {
+    belowModal.classList.remove('hidden');
+    modalWindow[i].classList.remove('hidden');
+  });
+} // for loop 
