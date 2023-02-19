f58bf0ee5e88cae164916b65cf76be226218d13c
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-02-19 10:05:27 -0600

    features: new desktop slider fot tolescro images introduction

---
+++ src/components/viewport/mediaBreak.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let viewPort = window.matchMedia("(max-width: 700px)");
+// dom elements
+const navBar = document.querySelector('.topBar');
+const view_DESKTOP = document.querySelector('.container-sliderTolescro')
+const view_MOBILE = document.querySelector('.wrapper-tolescro-screens')
+
+const mediaScreen = (y) => {
+  if (y.matches) {
+    view_DESKTOP.classList.add('hidden');
+    view_MOBILE.classList.remove('hidden');
+  } else {
+    view_DESKTOP.classList.remove('hidden');
+    view_MOBILE.classList.add('hidden');
+  }
+}
+mediaScreen(viewPort)
+viewPort.addListener(mediaScreen)
