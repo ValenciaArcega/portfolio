0ac51ffe69808f8a834eac9582484d7f7cca44f0
Luis Angel Valencia Arcega <valencia.arcega@gmail.com>
2023-02-25 13:13:02 -0600

    fixes: tolescro images extension was fixed on js file, also some style fixes on discover tolescro button

--- src/components/darkMode/appearance.js
+++ src/components/darkMode/appearance.js
@@ -6,24 +6,28 @@ const button_DARKMODE = document.querySelector('.topBar-btnDark');
 const button_LIGHTMODE = document.querySelector('.topBar-btnLight');
 
 const function_TURN_DARK = () => {
-  root.style.setProperty('--white','#121212');
+  root.style.setProperty('--white', '#121212');
   root.style.setProperty('--black', 'white');
-  root.style.setProperty('--bgCardKnowledges','#131313');
+  root.style.setProperty('--bgCardKnowledges', '#131313');
   root.style.setProperty('--mainColor', '#B1BFF7');
-  root.style.setProperty('--borderNavBar','#353535');
-  root.style.setProperty('--borderCardContact','#353535');
-  root.style.setProperty('--btnExpand','#1b1b1b');
+  root.style.setProperty('--borderNavBar', '#353535');
+  root.style.setProperty('--borderCardContact', '#353535');
+  root.style.setProperty('--btnExpand', '#1b1b1b');
+  root.style.setProperty('--btnTolescro', '#0D1E3E');
+  root.style.setProperty('--btnTolescroText', '#0A84FF');
   button_DARKMODE.classList.add('hidden');
   button_LIGHTMODE.classList.remove('hidden');
 };
 
 const function_TURN_LIGHT = () => {
-  root.style.setProperty('--white','white');
-  root.style.setProperty('--black','#121212');
+  root.style.setProperty('--white', 'white');
+  root.style.setProperty('--black', '#121212');
   root.style.setProperty('--mainColor', '#4263eb');
-  root.style.setProperty('--borderNavBar','transparent');
-  root.style.setProperty('--borderCardContact','#d9d9d9');
-  root.style.setProperty('--btnExpand','#f0f0f0');
+  root.style.setProperty('--borderNavBar', 'transparent');
+  root.style.setProperty('--borderCardContact', '#d9d9d9');
+  root.style.setProperty('--btnExpand', '#f0f0f0');
+  root.style.setProperty('--btnTolescro', '#E1E7F7');
+  root.style.setProperty('--btnTolescroText', '#0C80FE');
   button_LIGHTMODE.classList.add('hidden');
   button_DARKMODE.classList.remove('hidden');
 };
--- src/components/timers/sliderImages.js
+++ src/components/timers/sliderImages.js
@@ -2,11 +2,11 @@
 
 let imgCounter = 1;
 const imgTolescro = document.querySelector('.tolescro-screens-img');
-const labelImages = document.querySelector('.tolescro-screens-p')
+const labelImages = document.querySelector('.tolescro-screens-p');
 
 setInterval(() => {
   if (imgCounter > 3) imgCounter = 1;
-  imgTolescro.src = `src/assets/img/tolescro/tolescro-screen${imgCounter}.png`;
+  imgTolescro.src = `src/assets/img/tolescro/tolescro-screen${imgCounter}.jpg`;
   if (imgCounter === 1) labelImages.textContent = 'useful and complete sections';
   if (imgCounter === 2) labelImages.textContent = 'a powerful search';
   if (imgCounter === 3) labelImages.textContent = 'tips, slangs and much more';
