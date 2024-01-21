8486809cfd1336c12f752f54592cf0b5c976760f
ValenciaArcega <valencia.arcega@gmail.com>
2024-01-20 19:02:41 -0600

    features - new confetti effect and new skills section

--- src/scripts/showMail.js
+++ src/scripts/showMail.js
@@ -1,9 +1,12 @@
-'use strict';
+'use strict'
+import CanvasConfetti from 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.0/+esm'
 
-const btnShowMail = document.querySelector('.btnShowMail');
-const txt = document.querySelector('#myEmail');
+
+const btnShowMail = document.querySelector('.btnShowMail')
+const txt = document.querySelector('#myEmail')
 
 btnShowMail.addEventListener('click', function () {
-  txt.classList.remove('hidden');
-  btnShowMail.classList.add('hidden');
-});
+  txt.classList.remove('hidden')
+  btnShowMail.classList.add('hidden')
+  CanvasConfetti()
+})
